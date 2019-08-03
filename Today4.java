c2e673be7d560af10367ffa154a38f7af8ff03a3
Roop98 <roopranjank@gmail.com>
2019-08-03 14:57:58 +0300

    Another new one

---
+++ Today4.java
@@ -0,0 +1,11 @@
+package ee.sdacademy;
+
+public class Today4 {
+    public static void main(String[] args) {
+
+        Today3 today3 = new Today3();
+        today3.scanarray();
+        today3.printarrayleft();
+        today3.printarrayright();
+    }
+}
